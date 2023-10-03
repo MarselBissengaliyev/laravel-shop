cf55e3f0fcd67149f0e086db30b2705451341b2b
MarselBisengaliev <marselbisengaliev1@gmail.com>
2023-10-03 08:13:58 +0500

    update migrations (add to them user_id)

--- database/migrations/2023_10_03_030524_create_orders_table.php
+++ database/migrations/2023_10_03_030524_create_orders_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->integer('number');
             $table->string('status');
+            $table->integer('user_id');
             $table->timestamps();
         });
     }
