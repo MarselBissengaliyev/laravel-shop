c4baae40bf061656f508a2e7ff745d6d4c433be4
MarselBisengaliev <marselbisengaliev1@gmail.com>
2023-10-03 08:09:21 +0500

    create basic migrations

--- database/migrations/2023_10_03_030400_create_products_table.php
+++ database/migrations/2023_10_03_030400_create_products_table.php
@@ -11,12 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('user_name')->unique();
-            $table->string('password');
-            $table->string('role')->default('customer');
-            $table->rememberToken();
+            $table->string('name');
+            $table->string('description');
+            $table->float('price');
+            $table->string('picture_url');
             $table->timestamps();
         });
     }
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 };
--- database/migrations/2023_10_03_030650_create_order_products_table.php
+++ database/migrations/2023_10_03_030650_create_order_products_table.php
@@ -11,12 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('order_products', function (Blueprint $table) {
             $table->id();
-            $table->string('user_name')->unique();
-            $table->string('password');
-            $table->string('role')->default('customer');
-            $table->rememberToken();
+            $table->integer('order_id');
+            $table->integer('product_id');
             $table->timestamps();
         });
     }
@@ -26,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('order_products');
     }
 };
