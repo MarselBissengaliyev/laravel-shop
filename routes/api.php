a2f6cc6920cd3e0831c95ec772d5a1f9af72808b
MarselBisengaliev <marselbisengaliev1@gmail.com>
2023-10-03 20:20:03 +0500

    update routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +18,35 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->group(function () {
+    Route::prefix('auth')->group(function () {
+        Route::post('/sign-up', [AuthController::class, 'SignUp']);
+        Route::post('/sign-in', [AuthController::class, 'SignIn']);
+
+        Route::middleware('auth:sanctum')->group(function () {
+            Route::post('/logout', [AuthController::class, 'Logout']);
+        });
+    });
+
+    Route::prefix('products')->group(function () {
+        Route::get('/', [ProductController::class, 'GetProducts']);
+
+        Route::middleware(['auth:sanctum', 'is_admin'])->group(function () {
+            Route::get('/my', [ProductController::class, 'GetMyProducts']);
+            Route::post('/', [ProductController::class, 'CreateProduct']);
+            Route::put('/{product_id}', [ProductController::class, 'UpdateProductById']);
+            Route::delete('/{product_id}', [ProductController::class, 'DeleteProductById']);
+        });
+    });
+
+    Route::prefix('orders')->group(function() {
+        Route::middleware(['auth:sanctum', 'is_customer'])->group(function() {
+            Route::get('/my', [OrderController::class, 'GetMyOrders']);
+            Route::post('/', [OrderController::class, 'CreateOrder']);
+        });
+
+        Route::middleware(['auth:sanctum', 'is_admin'])->group(function () {
+        });
+        Route::get('/products/{product_id}', [OrderController::class, 'ShowOrdersOfMyProduct']);
+    });
 });
